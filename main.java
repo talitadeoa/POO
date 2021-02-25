3c71086100611edcfeaa34c7fb06b5948867a150
Talita Araújo <55101326+talitadeoa@users.noreply.github.com>
2021-02-25 17:00:19 -0300

    Update main.java

--- main.java
+++ main.java
@@ -1,7 +1,13 @@
 public class Alunos {
-
+    public string Nome; 
+    private double Cpf;
+    private int idade;
+    private double Altura;
+    private double Peso;
+    public Status;
 }
 
 public class Planos {
+    
     
 }
